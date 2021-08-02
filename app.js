2781eadfc7c83150ef761807ceb0368bda327a4c
alisoyunjov <62424497+alisoyunjov@users.noreply.github.com>
2021-08-02 14:28:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -38,14 +38,7 @@ app.use('/api', orders);
 app.use('/api/users', users);
 app.use('/api/vendors/', vendors);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
-
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 8082;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
